ac8591fabfaf25a1253f842786f062cefc8617f0
Denis Gutsulak <d.99113@gmail.com>
2023-07-17 09:40:01 +0300

    v3

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { searchRepositories } from './redux/actions';
 import RepositoryCard from './components/RepositoryCard';
--- src/components/RepositoryCard.tsx
+++ src/components/RepositoryCard.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import './RepositoryCard.css'; // Импортируем файл стилей
+import React, { useCallback } from 'react';
+import PropTypes from 'prop-types';
 
 const RepositoryCard = ({ repository }) => {
-  const handleProjectClick = () => {
+  const handleProjectClick = useCallback(() => {
     window.open(repository.html_url, '_blank');
-  };
+  }, [repository.html_url]);
 
-  const handleAuthorClick = () => {
+  const handleAuthorClick = useCallback(() => {
     window.open(repository.owner.html_url, '_blank');
-  };
+  }, [repository.owner.html_url]);
 
   return (
     <div className="repository-card">
@@ -20,6 +20,19 @@ const RepositoryCard = ({ repository }) => {
       <button onClick={handleAuthorClick}>View Author</button>
     </div>
   );
+};
+
+RepositoryCard.propTypes = {
+  repository: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    owner: PropTypes.shape({
+      login: PropTypes.string.isRequired,
+      html_url: PropTypes.string.isRequired,
+    }).isRequired,
+    stargazers_count: PropTypes.number.isRequired,
+    watchers_count: PropTypes.number.isRequired,
+    html_url: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default RepositoryCard;
